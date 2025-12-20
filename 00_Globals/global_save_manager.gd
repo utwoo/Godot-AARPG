09c6d08d08ec58022959102cdf54f2092b894010
utwoo <zhuxiang1212@hotmail.com>
2025-12-20 10:43:25 +0800

    Chapter 18: Save & Load for Items

--- 00_Globals/global_save_manager.gd
+++ 00_Globals/global_save_manager.gd
@@ -21,6 +21,7 @@ var current_save: Dictionary = {
 func save_game():
 	update_player_data()
 	update_scene_path()
+	update_item_data()
 	
 	var file := FileAccess.open( SAVE_PATH + "save.sav", FileAccess.WRITE )
 	var save_json = JSON.stringify( current_save )
@@ -44,6 +45,7 @@ func load_game():
 	
 	PlayerManager.set_player_position( Vector2 ( current_save.player.position_x,  current_save.player.position_y ) )
 	PlayerManager.set_player_health( current_save.player.hp, current_save.player.max_hp )
+	PlayerManager.INVENTORY_DATA.parse_save_data( current_save.items )
 	
 	await LevelManager.level_loaded
 	
@@ -67,4 +69,5 @@ func update_scene_path():
 			current_save.scene_path = p
 			break
 	
-	
+func update_item_data():
+	current_save.items = PlayerManager.INVENTORY_DATA.get_save_data()
